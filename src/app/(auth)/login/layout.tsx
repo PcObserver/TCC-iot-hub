5c7b25e45c45e2ae8a997845acd935a8ed3e93fe
Eduardo Donadon <dona.edu2@gmail.com>
2024-05-02 23:39:51 -0300

    feat: redirect to dashboard if is already authenticated in login page

---
+++ src/app/(auth)/login/layout.tsx
@@ -0,0 +1,21 @@
+import { cookies } from "next/headers";
+import { permanentRedirect } from "next/navigation";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const cookieStore = cookies()
+  const isAuthenticated = cookieStore.has('sessionToken') && cookieStore.has('refreshToken')
+
+  if (isAuthenticated) {
+    permanentRedirect('/dashboard')
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
