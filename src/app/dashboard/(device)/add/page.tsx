84070af08b97e53f7293e7c42480a7e2b193abdd
Eduardo Donadon <dona.edu2@gmail.com>
2024-05-20 22:20:04 -0300

    feat: conected device to api

--- src/app/dashboard/(device)/add/page.tsx
+++ src/app/dashboard/(device)/add/page.tsx
@@ -1,18 +1,39 @@
 "use client";
 
 import DeviceForm from "@/components/form/deviceForm";
+import { api } from "@/services/api";
 import { AddDeviceData } from "@/utils/interfaces";
+import { getCookie } from "cookies-next";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
+import { toast } from "sonner";
 
 
 
 
 export default function AddDevice() {
+  const router = useRouter()
+  const token = `Bearer ${getCookie('sessionToken')}`
+  const decoded = jwtDecode(token) as { user_id: string };
+  const userId = decoded?.user_id
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
     const data = Object.fromEntries(formData) as AddDeviceData
-    console.log({ data })
+    const deviceData = { ...data, user_id: userId, contribution_type: "Device" }
+
+    await api.post('/api/contributions/', deviceData, {
+      headers: {
+        'Authorization': token
+      }
+    }).then(() => {
+      toast.success('Comando criado!')
+      router.push('/dashboard')
+    }).catch(() => {
+      toast.error('Erro ao tentar criar comando.')
+    })
   }
 
   return (
