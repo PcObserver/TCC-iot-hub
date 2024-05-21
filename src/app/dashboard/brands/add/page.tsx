163ee1ee6d810b5ac81381dc5abbe4ddb29dbb10
Eduardo Donadon <dona.edu2@gmail.com>
2024-05-20 21:44:03 -0300

    feat: connect create brand to api

--- src/app/dashboard/brands/add/page.tsx
+++ src/app/dashboard/brands/add/page.tsx
@@ -1,18 +1,35 @@
 "use client";
 
 import BrandForm from "@/components/form/brandForm";
-import { Input } from "@/components/form/input";
 import { AddBrandData } from "@/utils/interfaces";
+import { jwtDecode } from "jwt-decode";
+import { getCookie } from "cookies-next";
 import { FormEvent } from "react";
+import { api } from "@/services/api";
+import { toast } from "sonner";
 
 
 
 export default function AddBrand() {
+  const token = `Bearer ${getCookie('sessionToken')}`
+  const decoded = jwtDecode(token) as { user_id: string };
+  const userId = decoded?.user_id
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
     const data = Object.fromEntries(formData) as AddBrandData
-    console.log({ data })
+    const brandData = { ...data, user_id: userId, contribution_type: "Brand" }
+
+    await api.post('/api/contributions/', brandData, {
+      headers: {
+        'Authorization': token
+      }
+    }).then(() => {
+      toast.success('Marca criada!')
+    }).catch(() => {
+      toast.error('Erro ao tentar criar marca.')
+    })
   }
 
   return (
