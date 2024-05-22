0bee886d3a0754214453b42fab32bcc2ca3d3b56
dgs-small <lucas9667@hotmail.com>
2024-05-22 09:59:24 -0300

    Finished dropdown style

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,5 +1,5 @@
 import { Menu, MenuButton, MenuItems, Transition } from '@headlessui/react'
-import { Wifi } from 'lucide-react'
+import { EllipsisVertical } from 'lucide-react'
 
 import { ReactNode } from 'react'
 
@@ -8,9 +8,7 @@ export default function Dropdown({ children }: { children: ReactNode }) {
         <div className="w-52 text-right">
             <Menu>
                 <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
-                    Options
-
-                    <Wifi className="size-4 fill-white/60" />
+                    <EllipsisVertical className="size-4 fill-white/60" />
                 </MenuButton>
                 <Transition
                     enter="transition ease-out duration-75"
@@ -22,7 +20,7 @@ export default function Dropdown({ children }: { children: ReactNode }) {
                 >
                     <MenuItems
                         anchor="bottom end"
-                        className="w-52 origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white [--anchor-gap:var(--spacing-1)] focus:outline-none"
+                        className="w-52 origin-top-right rounded-xl border border-black/10 bg-white  p-1 text-sm/6 text-white [--anchor-gap:var(--spacing-1)] focus:outline-none"
                     >
                         {children}
                     </MenuItems>
